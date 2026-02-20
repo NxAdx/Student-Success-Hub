dee0347406b6fbe17725e048cab9975024d74522
“[NxAdx]” <[lokhandeaadarsh28@gmail.com]>
2026-02-21 00:07:00 +0530

    Fix: Explicit STATICFILES_DIRS and verbose logging

--- student_success_hub/settings.py
+++ student_success_hub/settings.py
@@ -154,7 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'theme' / 'static',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (User-uploaded content)
